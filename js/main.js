d44c5e2005972e85b01dfd85c0ee332bd8135cf1
Cyrus-Nebula <xzh.6.8.xzh@hotmail.com>
2026-08-24 20:05:38 +0800

    Delete js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function goSearch(){
-    let kw = document.getElementById("searchInput").value.trim();
-    if(kw){
-        window.location.href="search.html?kw="+encodeURIComponent(kw);
-    }
-}
-function filterList(){
-    alert("筛选功能：你可以后续自行扩展JS逻辑，现在仅做演示");
-}
